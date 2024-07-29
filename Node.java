a6b7c04dda15ebb0926aaef470ef90d628a4cfef
GARGI SINGH <160568077+gargi1p@users.noreply.github.com>
2024-07-29 23:14:59 +0530

    first commit

---
+++ Node.java
@@ -0,0 +1,7 @@
+public class Node {
+   
+    int data;
+   // the next variable is of type Node because it holds a reference to another Node object, rather than a primitive type like int.
+	Node next;
+
+}
